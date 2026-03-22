4f3535e793197d319a9a4afd461dd16a9208e48b
Joseppius <cathcartj08@gmail.com>
2026-03-22 16:42:35 +0000

    Refactored ToolTip rendering

--- src/ToolTip.js
+++ src/ToolTip.js
@@ -2,27 +2,20 @@ import { state } from "./state.js"
 
 export class ToolTip {
     constructor(text) {
-        this.svgContainer = state.svg.append('svg').attr('class', 'tooltipcontainer');
+        this.svgContainer = state.svg.append('svg').attr('class', 'ToolTipContainer');
         this.svgContainer.raise();
 
         const foreignObject = this.svgContainer.append('foreignObject')
             .attr('pointer-events', 'none')
-            .attr('width', 120 * state.scale)
-            .attr('height', 66 * state.scale)
+            .attr('width', 10000 * state.scale)
+            .attr('height', 10000 * state.scale)
             .style('position', 'relative')
 
         const toolTip = foreignObject.append('xhtml:div')
-            .attr('class', 'tooltip')
-            .style('width', '100%')
-            .style('height', '100%')
-            .style('border-image-width', (state.scale * 10).toString() + "px")
+            .attr('class', 'ToolTip')
             .style('position', 'absolute')
-        foreignObject.append('xhtml:div')
-            .attr('class', 'tooltipFrame')
-            .style('width', '100%')
-            .style('height', '100%')
-            .style('border-image-width', (state.scale * 10).toString() + "px")
-            .style('position', 'absolute')
+            .style('padding', state.scale * 11 + "px");
+        toolTip.node().style.setProperty('--border-image-width', (state.scale * 10).toString() + "px");
 
         this.svgContainer.attr('x', (Math.trunc(state.mouseX / state.scale) * state.scale) - (1 * state.scale));
         this.svgContainer.attr('y', (Math.trunc(state.mouseY / state.scale) * state.scale) - (24 * state.scale));
@@ -34,65 +27,18 @@ export class ToolTip {
         })
 
         // Text Time!
-        this.svgContainer.append('text').attr('class', 'minecraftTextShadow')
-                .text(text)
-                .attr('font-size', 7 * state.scale)
-                .attr('y', 19 * state.scale)
-                .attr('x', 13 * state.scale)
-                .attr('pointer-events', 'none')
-                .attr('fill', '#3e3e3e')
-        this.svgContainer.append('text').attr('class', 'minecraftText')
-                .text(text)
-                .attr('font-size', 7 * state.scale)
-                .attr('y', 18 * state.scale)
-                .attr('x', 12 * state.scale)
-                .attr('pointer-events', 'none')
-                .attr('fill', 'white')
+        toolTip.append('xhtml:p').attr('class', 'minecraftText').text(text)
+        toolTip.append('xhtml:div').attr('class', 'minecraftText')
+                .style('line-height', state.scale * 12 + 'px')
+                .html('<br/>')
+        toolTip.append('xhtml:p').attr('class', 'minecraftText').text(text)
+        toolTip.append('xhtml:p').attr('class', 'minecraftText').text(text)
 
-        this.svgContainer.append('text').attr('class', 'minecraftTextShadow')
-                .text("When on head:")
-                .attr('font-size', 7 * state.scale)
-                .attr('y', 33 * state.scale)
-                .attr('x', 13 * state.scale)
-                .attr('pointer-events', 'none')
-                .attr('fill', '#2a2a2a')
-        this.svgContainer.append('text').attr('class', 'minecraftText')
-                .text("When on head:")
-                .attr('font-size', 7 * state.scale)
-                .attr('y', 32 * state.scale)
-                .attr('x', 12 * state.scale)
-                .attr('pointer-events', 'none')
-                .attr('fill', '#a8a8a8')
+        toolTip.selectAll('p')
+                .style('font-size', 7 * state.scale + "px")
+                .style('text-shadow', state.scale + "px " + state.scale + "px " + "#3e3e3e")
+                .style('padding', state.scale + "px");
 
-        this.svgContainer.append('text').attr('class', 'minecraftTextShadow')
-                .text("+2 armour toughness")
-                .attr('font-size', 7 * state.scale)
-                .attr('y', 42 * state.scale)
-                .attr('x', 13 * state.scale)
-                .attr('pointer-events', 'none')
-                .attr('fill', '#15153e')
-        this.svgContainer.append('text').attr('class', 'minecraftText')
-                .text("+2 armour toughness")
-                .attr('font-size', 7 * state.scale)
-                .attr('y', 41 * state.scale)
-                .attr('x', 12 * state.scale)
-                .attr('pointer-events', 'none')
-                .attr('fill', '#5454fc')
-
-        this.svgContainer.append('text').attr('class', 'minecraftTextShadow')
-                .text("+2 armour")
-                .attr('font-size', 7 * state.scale)
-                .attr('y', 51 * state.scale)
-                .attr('x', 13 * state.scale)
-                .attr('pointer-events', 'none')
-                .attr('fill', '#15153e')
-        this.svgContainer.append('text').attr('class', 'minecraftText')
-                .text("+2 armour")
-                .attr('font-size', 7 * state.scale)
-                .attr('y', 50 * state.scale)
-                .attr('x', 12 * state.scale)
-                .attr('pointer-events', 'none')
-                .attr('fill', '#5454fc')
     }
 
     delete() {
