badb3138e2dda76b7557cc103140ea88f888e0a5
Joseppius <cathcartj08@gmail.com>
2026-03-23 00:00:15 +0000

    Added knockback stick to inventory

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ state.svg.on('mousemove', (event) => {
 })
 
 const inventory = new Inventory(grid);
-grid.drawGrid();
+// grid.drawGrid();
 
 const helmet = new Item("./src/assets/textures/item/diamond_helmet.png", ItemType.HELMET, 2, 3, null, "Diamond Helmet");
 const chestplate = new Item("./src/assets/textures/item/diamond_chestplate.png", ItemType.CHESTPLATE, 2, 8, ["Protection IV", "Thorns III", "Mending"], "Diamond Chestplate");
@@ -25,6 +25,7 @@ const leggings = new Item("./src/assets/textures/item/diamond_leggings.png", Ite
 const boots = new Item("./src/assets/textures/item/diamond_boots.png", ItemType.BOOTS, 2, 3, null, "Diamond Boots");
 const sword = new Item("./src/assets/textures/item/diamond_sword.png", ItemType.WEAPON, 1.6, 10, ["Sharpness V", "Mending"], "Diamond Sword");
 const string = new Item("./src/assets/textures/item/string.png", ItemType.DEFAULT, null, null, null, "String");
+const stick = new Item("./src/assets/textures/item/stick.png", ItemType.DEFAULT, null, null, ["Knockback enchantment.level.255"], "Stick");
 
 inventory.slots[5].setItem(helmet);
 inventory.slots[6].setItem(chestplate);
@@ -32,6 +33,7 @@ inventory.slots[7].setItem(leggings);
 inventory.slots[8].setItem(boots);
 inventory.slots[36].setItem(sword);
 inventory.slots[44].setItem(string);
+inventory.slots[43].setItem(stick);
 
 // TESTING GROUNDS
 
