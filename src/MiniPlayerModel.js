e527b1b05793ef78c9e98d4fc03815d20972fb67
Joseppius <cathcartj08@gmail.com>
2026-03-24 14:11:05 +0000

    Polished inventory player model

--- src/MiniPlayerModel.js
+++ src/MiniPlayerModel.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { TessellateModifier } from 'three/addons/modifiers/TessellateModifier.js';
 import { state } from "./state.js";
 
 export class MiniPlayerModel {
@@ -20,18 +21,21 @@ export class MiniPlayerModel {
         // Create renderer
         const renderer = new THREE.WebGLRenderer( {antialias: false});
         renderer.setSize(canvasX, canvasY);
+        // renderer.setClearColor( 0x88ffff, 1 );
         foreignObject.node().appendChild(renderer.domElement);
         
-        // Create camera
-        const fov = 70; const aspect = canvasX / canvasY; const near = 0.1; const far = 100;
-        const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-        camera.position.z = 2.0; camera.position.y = 0;
+        const scale = 57.5 * state.scale;
+        const camera = new THREE.OrthographicCamera(canvasX / -scale, canvasX / scale, canvasY / scale, canvasY / -scale, 1, 3);
+        camera.position.z = 2.0; camera.position.y = -0.05;
         
         // Create scene
         const scene = new THREE.Scene();
 
         // Create & Load player model
         let object = null;
+        let head = null;
+        let left_arm = null;
+        let right_arm = null;
         const loader = new GLTFLoader();
         loader.load(
             './src/assets/models/MinecraftPlayer/Slim/Untitled.gltf',
@@ -39,10 +43,20 @@ export class MiniPlayerModel {
                 gltf.scene.traverse((child) => {
                     if (child.isMesh) {
                         child.material.depthWrite = true;
+                        child.renderOrder = 1;
                     }
+                    if (child.name == "Head")
+                        head = child;
+                    if (child.name == "ArmLeftUpper")
+                        left_arm = child;
+                    if (child.name == "ArmRightUpper")
+                        right_arm = child;
+                    console.log(child.name)
                 });
                 // If file is loaded, add it to scene
                 object = gltf.scene;
+                // const modifier = new TessellateModifier( 8, 8 );
+                // object = modifier.modify(object);
                 scene.add(object);
             }, null ,
             (error) => {
@@ -54,6 +68,13 @@ export class MiniPlayerModel {
         // Start the render loop
         const animate = () => {
             requestAnimationFrame(animate);
+
+            if (right_arm != null && left_arm != null) {
+                const d = new Date();
+                let time = d.getSeconds() * 1000 + d.getMilliseconds();
+                left_arm.rotation.z = (Math.sin(time / 750) - 1) / 15;
+                right_arm.rotation.z = (Math.sin(time / 750) - 1) / -15;
+            };
             renderer.render(scene, camera);
         };
         animate();
@@ -72,12 +93,14 @@ export class MiniPlayerModel {
                 const ax = 0.8; // Bell function height at x = 0
                 const ay = 1.0;
                 const x = Math.max(Math.min((state.mouseX - xPos - canvasX / 2) / 500, ax), -ax);
-                const y = Math.max(Math.min((state.mouseY - yPos - canvasY / 2) / 500, ay), -ay);
+                const y = Math.max(Math.min((state.mouseY - yPos - canvasY / 4) / 500, ay), -ay);
                 const bellX = 1 / (1 + ((x / ax)*(x / ax)));
                 const bellY = 1 / (1 + ((y / ay)*(y / ay)));
                 object.rotation.y = x * bellX;
                 object.rotation.x = y * bellY;
-                console.log("mouse x = " + x + "   \t rotation x = " + x * bellX );
+                head.rotation.y = x * bellX;
+                head.rotation.x = y * bellY;
+                // console.log("mouse x = " + x + "   \t rotation x = " + x * bellX );
             }
         })
 
