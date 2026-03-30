85a349e349895c7929dcfc0bbb4f446578311da2
Sonryle <cathcartj08@gmail.com>
2026-03-30 23:35:36 +0100

    Added .gitignore

--- server.cjs
+++ server.cjs
@@ -3,10 +3,10 @@ const app = express()
 const port = 5137;
 
 app.get('/', (req, res) => {
-  res.sendfile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
 })
 
-app.use(express.static('/'))
+app.use(express.static('./'))
 
 app.listen(port, () => {
   console.log(`Endgame listening on port ${port}`)
