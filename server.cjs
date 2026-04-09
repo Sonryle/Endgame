8d46498608257e6a8500ea9b69a5b39784777b7f
Sonryle <cathcartj08@gmail.com>
2026-04-09 10:15:16 +0100

    Cleaning up & continuing Skin Stealer Backend Code

--- server.cjs
+++ server.cjs
@@ -7,19 +7,36 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/skin/:username', async (req, res) => {
+
+    // Fetch UUID from Username
     let response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${req.params.username}`);
+    if (!response.ok)
+        console.log(`Response to requesting UUID from Username was not OK: ${response.statusText}`);
     let data = await response.json();
-    const UUID = data.id
-    response = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${UUID}`);
+
+    // Fetch User Data with UUID
+    response = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${data.id}`);
+    if (!response.ok)
+        console.log(`Response to requesting User Data using UUID was not OK: ${response.statusText}`);
     data = await response.json();
-    const value = data.properties[0].value;
-    console.log(value);
-    const decodedString = atob(value);
-    const lastJSON = JSON.parse(decodedString);
-    const textures = lastJSON.textures;
-    const skin = textures.SKIN;
-    const URL = skin.url;
-    console.log(URL);
+
+    // Decoded skin information
+    const textureInfo = data.properties[0].value;
+    const skinInfo = JSON.parse(atob(textureInfo));
+    const skin = skinInfo.textures.SKIN;
+
+    // Find PlayerType
+    let playerType = "WIDE";
+    if (typeof skin.metadata != "undefined")
+        playerType = "SLIM";
+    console.log(playerType);
+    console.log(skin.url);
+
+    // response = await fetch(obj.skinURL);
+    //
+    // const blobImage = await response.blob();
+    //
+    // const href = URL.createObjectURL(blobImage);
 });
 
 app.use(express.static('./'))
